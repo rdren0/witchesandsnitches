fe744010219cfb296b92403e54b6766435bef952
Rachael <rdrennan0@gmail.com>
2025-10-29 18:11:44 -0600

    allow resilent feat more than once

--- src/SharedData/standardFeatData.js
+++ src/SharedData/standardFeatData.js
@@ -1282,14 +1282,16 @@ export const standardFeats = [
     name: "Resilient",
     preview: "Gain proficiency in one saving throw. +1 to that ability.",
     description: [
-      "Ability Score Increase: Choose one ability in which you lack saving throw proficiency. Increase the chosen ability score by 1, to a maximum of 20.",
+      "Choose one ability score. You gain the following benefits:",
+      "Ability Score Increase: Increase the chosen ability score by 1, to a maximum of 20.",
       "Saving Throw Proficiency: You gain saving throw proficiency with the chosen ability.",
+      "Repeatable: You can take this feat more than once, but you must choose a different ability score each time.",
     ],
     benefits: {
       abilityScoreIncrease: { type: "choice_any", amount: 1 },
       skillProficiencies: [],
       expertise: [],
-      savingThrowProficiencies: [{ type: "choice", count: 1 }],
+      savingThrowProficiencies: ["linked_to_ability_choice"],
       resistances: [],
       immunities: [],
       speeds: {},
@@ -1297,6 +1299,8 @@ export const standardFeats = [
       spellcasting: {},
       specialAbilities: [],
     },
+    repeatable: true,
+    repeatableKey: "savingThrow",
   },
   {
     name: "Second Chance",
